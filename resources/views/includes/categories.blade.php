714db25b81c80c5ebfa436515d10ff8b0ef275f0
unknown <amitsingh17051@gmail.com>
2022-08-29 18:57:19 +0530

    adde main navigation and category navigatiopn

---
+++ resources/views/includes/categories.blade.php
@@ -0,0 +1,18 @@
+<div class="cate-list-menu pt-5 pb-5">
+    <ul class="cat-list flex gap-10">
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="/categories" class="p-2 lg:px-4 md:mx-2 text-white rounded bg-indigo-600">All Categories</a></li>
+    </ul>
+</div>
+
