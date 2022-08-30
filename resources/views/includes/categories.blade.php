73fa053a550b0560941215c534c8a2400172102e
unknown <amitsingh17051@gmail.com>
2022-08-30 16:25:12 +0530

    no change

--- resources/views/includes/categories.blade.php
+++ resources/views/includes/categories.blade.php
@@ -1,4 +1,4 @@
-<div class="cate-list-menu pt-5 pb-5">
+<div class="cate-list-menu pt-5 pb-5 sm:overflow-x-scroll md:overflow-x-scroll lg:overflow-x-auto">
     <ul class="cat-list flex gap-10">
         <li><a href="#">Category</a></li>
         <li><a href="#">Category</a></li>
@@ -15,4 +15,10 @@
         <li><a href="/categories" class="p-2 lg:px-4 md:mx-2 text-white rounded bg-indigo-600">All Categories</a></li>
     </ul>
 </div>
+
+<style>
+    .cate-list-menu li {
+        white-space: nowrap;
+    }
+</style>
 
